f0f4bb3804c066592a48eea4c42c39d8a8f6a0e3
Irfansyah001 <syahirfan545@gmail.com>
2025-11-06 09:14:43 +0700

    feat: Create NavHost and DataApp Composable
    Adds the basic structure for the application's navigation.
    
    - Defines an enum `Navigasi` with destinations `Formulir` and `Detail`.
    - Creates a new composable function `DataApp` which will contain the `NavHost`.
    - The `DataApp` function initializes a `rememberNavController`.

--- app/src/main/java/com/example/questnavigasiui_223/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui_223/Navigasi.kt
@@ -1,6 +1,22 @@
 package com.example.questnavigasiui_223
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
 enum class Navigasi {
     Formulir,
     Detail
 }
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+) {
+    }
+}
+)
